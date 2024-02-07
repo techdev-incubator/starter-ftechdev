f0b5b3d1f969346bca9c87a308d07e2033d0e516
angga <anggaprabes@gmail.com>
2024-02-08 02:01:13 +0700

    import-export @packages/ui

--- README.md
+++ README.md
@@ -5,5 +5,6 @@ Bootstrapping remix-app and next-app projects using Turbo
 [GSlide](https://docs.google.com/presentation/d/1Z7M_SsuJ-f_cIDkXsE6JKA0LsBiVBsc00dlKWqPQM2I/edit#slide=id.g2683841e23b_0_15)
 
 
-### Licensing
+## Licensing
+
 This source code is unlicensed, and free for anyone to use as they please. Any effort to improve source or explore the concept further is encouraged!
---
+++ packages/ui/src/card.tsx
@@ -0,0 +1,43 @@
+import * as React from 'react';
+
+type Props = React.PropsWithChildren<{
+    filepath: string
+}>
+
+export function Card({ children, filepath }: Props): JSX.Element {
+    return (
+        <div
+            style={{
+                borderRadius: 8,
+                background: "#eee",
+                border: "1.5px solid #ccc",
+                position: "relative",
+                minHeight: 80,
+                paddingInline: 8,
+                paddingTop: 40,
+                paddingBottom: 8,
+
+            }}
+        >
+            <div
+                style={{
+                    borderRadius: 8,
+                    background: "#ddd",
+                    border: "1.5px solid #bbb",
+                    position: "absolute",
+                    left: 8,
+                    top: -13,
+                    height: 20,
+                    paddingInline: 8,
+                    paddingBlock: 4,
+                    minWidth: 80,
+                    fontWeight: 600,
+                    color: "#2a2a2a"
+                }}
+            >
+                {filepath}
+            </div>
+            {children}
+        </div>
+    );
+}
